2469370e3c98c3f7615315ef780387c182594aae
Krzysztof Biskup <k.biskup.ns@gmail.com>
2020-01-03 06:58:16 +0100

    corrected

--- components/Search.js
+++ components/Search.js
@@ -32,22 +32,6 @@ render: function() {
              value={this.state.searchTerm}
             />
   },
-	
-	function () {
-		var styles = {
-			fontSize: '1.5em',
-				width: '90%',
-				maxWidth: '350px'
-		};
-	
-	return <input
-						type="text"
-						onChange={this.handleChange}
-						placeholder="Tutaj wpisz wyszukiwaną frazę"
-						style={styles}
-						value={this.state.searchTerm}
-					/>
-	}
 });
 
 //console.log(Search);
